de04bde3845c642d28aa4453ca9b2ff524a40db3
Konstatos <k@rochev.ru>
2021-07-18 12:38:54 +0300

    Расширения редактора

---
+++ Assets/Scripts/Editor/LookAtPointEditor.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.Character.Health;
+using UnityEditor;
+using UnityEngine;
+
+//[CustomEditor(typeof(HealthComponent))]
+[CanEditMultipleObjects]
+public class LookAtPointEditor : Editor
+{
+    private SerializedProperty lookAtPoint;
+
+    void OnEnable()
+    {
+        lookAtPoint = serializedObject.FindProperty("Target");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(lookAtPoint);
+        serializedObject.ApplyModifiedProperties();
+        EditorGUILayout.LabelField("(Текст)");
+        EditorGUILayout.ColorField(Color.red);
+    }
+}
